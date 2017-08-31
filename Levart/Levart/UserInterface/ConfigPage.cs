0b7e3c89f64c8e3a1df53bb626f029c0579e09bb
Jan-Philipp Voß <jan.philipp.voss@gmail.com>
2017-08-31 12:16:41 +0200

    Added XAMLs for UI

---
+++ Levart/Levart/UserInterface/ConfigPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Levart.UserInterface
+{
+    public class ConfigPage : ContentPage
+    {
+        public ConfigPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
