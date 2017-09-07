c6b81c216169e3c676bd65df6a9d97f17fd0833e
Marcel <marcel.schmidt89@web.de>
2017-09-07 13:54:43 +0200

    Listview with Databinding

--- Levart/Levart/UserInterface/MainPage.xaml.cs
+++ Levart/Levart/UserInterface/MainPage.xaml.cs
@@ -9,20 +9,34 @@ using Xamarin.Forms;
 namespace Levart
 {
 	public partial class MainPage : ContentPage
-	{
+    {
+        public class Product {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public decimal UnitCost { get; set; }
+        }
 
+        public static ObservableCollection<Product> ProductList = new ObservableCollection<Product> {
+            new Product { ID=1, Name="Widget", UnitCost=19.99m},
+            new Product { ID=2, Name="Whatzit", UnitCost=29.99m},
+            new Product { ID=3, Name="Gadget", UnitCost=39.99m},
+            new Product { ID=4, Name="Cog", UnitCost=49.99m},
+            new Product { ID=5, Name="Sprocket", UnitCost=59.99m}
+        };
 
-        public static ObservableCollection<string> ObservableStringList = new ObservableCollection<string>
-        {
+        public static ObservableCollection<string> ObservableStringList = new ObservableCollection<string> {
             "One", "Two", "Three", "Four"
         };
 
-        public MainPage()
-        {
+
+        public MainPage() {
             InitializeComponent();
 
+
+            simpleListView.ItemsSource = ObservableStringList;
+            boundListView.ItemsSource = ProductList;
             
-            listView.ItemsSource = ObservableStringList;
+
         }
 	}
 }
