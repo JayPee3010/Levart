422477358398ee2cedbc31766ae94593109eb13c
Jate <jasminteller@yahoo.com>
2017-09-11 13:07:05 +0200

    DetailPage - Code.

---
+++ Levart/Levart.iOS/DetailPage.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Levart.iOS
+{
+    public partial class DetailPage : UIPageViewController
+    {
+        public DetailPage (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
--- Levart/Levart/App.xaml.cs
+++ Levart/Levart/App.xaml.cs
@@ -2,36 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Levart.UserInterface;
 using Xamarin.Forms;
 
 namespace Levart
 {
-	public partial class App : Application
-	{
-		public App ()
-		{
-			InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new NavigationPage(new MainPage());
-
-		}
-
+            //MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new DetailPage());
 
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
