1e5f20a900d55bb0e6b892ed7bbfd43ed1b7ce78
Jate <jasminteller@yahoo.com>
2017-09-14 13:02:44 +0200

    DetailPage

--- Levart/Levart/UserInterface/DetailPage.xaml.cs
+++ Levart/Levart/UserInterface/DetailPage.xaml.cs
@@ -4,53 +4,14 @@ using System.Collections.Generic;
 using UIKit;
 using Xamarin.Forms;
 using System.Threading.Tasks;
-using UIKit;
 using Foundation;
 
 namespace Levart.UserInterface
 {
     public partial class DetailPage : ContentPage
     {
-        public class PicturePickerImplementation : IPicturePicker
-        {
-            TaskCompletionSource<Stream> taskCompletionSource;
-            UIImagePickerController imagePicker;
-
-            void OnImagePickerFinishedPickingMedia(object sender, UIImagePickerMediaPickedEventArgs args)
-            {
-                UIImage image = args.EditedImage ?? args.OriginalImage;
-
-                if (image != null)
-                {
-                    // Convert UIImage to .NET Stream object
-                    NSData data = image.AsJPEG(1);
-                    Stream stream = data.AsStream();
-
-                    // Set the Stream as the completion of the Task
-                    taskCompletionSource.SetResult(stream);
-                }
-                else
-                {
-                    taskCompletionSource.SetResult(null);
-                }
-                imagePicker.DismissModalViewController(true);
-            }
-
-            void OnImagePickerCancelled(object sender, EventArgs args)
-            {
-                taskCompletionSource.SetResult(null);
-                imagePicker.DismissModalViewController(true);
-            }
-
-            public Task<Stream> GetImageStreamAsync()
-            {
-                throw new NotImplementedException();
-            }
-
-            protected internal DetailPage()
-            {
-
-            }
-        }
-    }
+        image.Source = Device.OnPlatform(
+                    iOS: ImageSource.FromFile("Images/corgi.png"),
+            Android:  ImageSource.FromFile("corgi.png"),
+            WinPhone: ImageSource.FromFile("Images/corgi.png"));    }
 }
